6bddba58e5ff1cd0b3125e91d340ae57db334b56
Brandon-Ritchie <ritchietrumpet@gmail.com>
2024-01-10 07:22:42 -0500

    Add January 9th update

--- src/routes/war-on-music/WarOnMusic.tsx
+++ src/routes/war-on-music/WarOnMusic.tsx
@@ -15,6 +15,33 @@ export default () => {
       <h1>Youngstown State University’s War on Music</h1>
 
       <section>
+        <h2>Update - January 9th, 2023, 11:00pm EST</h2>
+        <Paragraph>
+          Two simultaneous events are taking place Wednesday, January 10, at
+          4pm.
+        </Paragraph>
+        <Paragraph>
+          Online, Acting Provost Jennifer Pintar will give a presentation to
+          YSU's Academic Senate, attempting to justify cutting Dana's (and
+          other) programs. This can be attended at the following link:{" "}
+          <a
+            className="underline"
+            href="https://ysu.webex.com/ysu/j.php?MTID=m213476a1a586953944920d5448cdf2ad"
+          >
+            https://ysu.webex.com/ysu/j.php?MTID=m213476a1a586953944920d5448cdf2ad
+          </a>
+        </Paragraph>
+        <Paragraph>Meeting number: 2313 583 2801</Paragraph>
+        <Paragraph>Password: K9vJ5jnqS3n</Paragraph>
+        <Paragraph>
+          In person, we'll have a raucous celebration of Dana and all things
+          music, outside YSU's Tod Hall. Come make great noise, and bring
+          #lovemusic and #savedana signs.
+        </Paragraph>
+        <Paragraph>If you want, come to both at once!</Paragraph>
+      </section>
+
+      <section>
         <h2>Update - January 8th, 2023, 10:00pm EST</h2>
 
         <Paragraph>
