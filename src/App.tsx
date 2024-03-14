e46d36be26587c235014f0af1a92250c88f05d33
Brandon-Ritchie <ritchietrumpet@gmail.com>
2024-03-14 11:50:06 -0400

    "remove" the site for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,39 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import PageLayout from "./lib/components/PageLayout.tsx";
-import Root from "./routes/Root.tsx";
-import Testimonials from "./routes/testimonials/Testimonials.tsx";
-import Testimonial from "./routes/testimonials/[name]/IndividualTestimonial.tsx";
-import WhatCanIDo from "./routes/what-can-i-do/WhatCanIDo.tsx";
-import WarOnMusic from "./routes/war-on-music/WarOnMusic.tsx";
 
+// const router = createBrowserRouter([
+//   {
+//     path: "",
+//     element: <PageLayout />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Root />,
+//       },
+//       {
+//         path: "/war-on-music",
+//         element: <WarOnMusic />,
+//       },
+//       {
+//         path: "/testimonials",
+//         element: <Testimonials />,
+//       },
+//       {
+//         path: "/testimonials/:name",
+//         element: <Testimonial />,
+//       },
+//       {
+//         path: "/what-can-i-do",
+//         element: <WhatCanIDo />,
+//       },
+//     ],
+//   },
+// ]);
+
+// This is in response to the current faculty requesting to have the site removed, at least temporarily.
 const router = createBrowserRouter([
   {
-    path: "",
-    element: <PageLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Root />,
-      },
-      {
-        path: "/war-on-music",
-        element: <WarOnMusic />,
-      },
-      {
-        path: "/testimonials",
-        element: <Testimonials />,
-      },
-      {
-        path: "/testimonials/:name",
-        element: <Testimonial />,
-      },
-      {
-        path: "/what-can-i-do",
-        element: <WhatCanIDo />,
-      },
-    ],
+    path: "*",
+    element: null,
   },
 ]);
 
